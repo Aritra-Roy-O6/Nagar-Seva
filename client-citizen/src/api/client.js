2ccdfb048b814d8273e40738ba901abc5d36a5ec
Aritra-Roy-O6 <contact.aritra2006@gmail.com>
2025-09-16 23:46:55 +0530

    deploy config client-admin

--- client-citizen/src/api/client.js
+++ client-citizen/src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Replace 'YOUR_COMPUTER_IP_ADDRESS' with the actual IP address you found.
 // The port must match the port your backend server is running on (e.g., 3001).
 const apiClient = axios.create({
-  baseURL: 'http://192.168.43.242:3001/api',
+  baseURL: 'https://nagar-seva-1.onrender.com/api',
 });
 
 export default apiClient;
