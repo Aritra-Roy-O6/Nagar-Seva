bb11fcf3fa0e97f43b48550fe8289384e88c4073
Aritra-Roy-O6 <contact.aritra2006@gmail.com>
2025-09-17 05:41:25 +0530

    deploy fix

--- client-admin/vite.config.js
+++ client-admin/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+       rollupOptions: {
+      external: [],
+    }
   }
 })
